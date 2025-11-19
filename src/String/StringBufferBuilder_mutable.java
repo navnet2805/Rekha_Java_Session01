cc904466857a392b18b6f3fc109222db2d8eb6fc
Naveen <naveen.rp.2805@gmail.com>
2025-11-19 10:14:19 +0530

    Commit 19

---
+++ src/String/StringBufferBuilder_mutable.java
@@ -0,0 +1,21 @@
+package String;
+
+public class StringBufferBuilder_mutable {
+
+    public static void main(String[] args) {
+      // StringBuilder sb =new StringBuilder("Java");
+       StringBuffer sb = new StringBuffer("Java");
+        System.out.println(sb.append("Program"));
+        System.out.println(sb.reverse());
+
+
+        /*
+        append() -- same as concat() in string
+        insert() -
+        delete()
+        reverse()
+         */
+
+
+    }
+}
---
+++ src/String/String_immutable.java
@@ -0,0 +1,25 @@
+package String;
+
+public class String_immutable {
+    public static void main(String[] args) {
+        String s = "Java"; //scp
+        String s1 = new String("Prog");
+        s.concat("Program");
+        System.out.println(s);
+
+
+//        System.out.println(s + "Program");
+
+
+
+
+
+        /*
+        System.out.println(s + 2);// + will concat
+        System.out.println(1 + 2); // + will addition
+        System.out.println("1" + 2); //concat
+
+         */
+
+    }
+}
